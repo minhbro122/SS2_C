744265b33d93967de41d4aaeb7c156bf52db438b
minhbro122 <Lynhan.zip.zip.zip@gmail.com>
2024-11-20 17:07:32 +0700

    Add files via upload

---
+++ SS2_Bài5.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h> 
+
+int main(){
+	int chieu_dai=9;
+	
+
+	int chieu_rong=4; 
+	
+	int chu_vi=(chieu_dai+chieu_rong)*2; 
+	
+	int dien_tich=chieu_dai*chieu_rong; 
+	
+	
+	printf("chieu dai cua hinh chu nhat la %d\n",chieu_dai); 
+	
+	printf("chieu rong cua hinh chu nhat la %d\n",chieu_rong);
+	
+	printf("chu vi cua hinh chu nhat la %d\n",chu_vi);
+	
+	printf("dien tich cua hinh chu nhat la %d\n",dien_tich);
+	
+	return 0; 
+}
