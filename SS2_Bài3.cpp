b24d2a29fcb51f67996e99de39a780966d5e87c9
minhbro122 <Lynhan.zip.zip.zip@gmail.com>
2024-11-20 16:35:15 +0700

    Add files via upload

---
+++ SS2_Bài3.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h> 
+
+int main (){
+	
+		int so1=10;
+	int so2=5;
+	
+	int tong= so1+so2;
+	printf ("phep cong la %d \n",tong);
+
+ int hieu=so1-so2;
+ printf("phep tru la %d \n",hieu);
+ 
+ 
+ int tich=so1*so2;
+ printf ("phep nhan la %d \n",tich);
+ 
+ 
+ 
+ int thuong=so1/so2;
+ printf("phep chia la %d \n",thuong);
+	
+
+	return 0; 
+	
+}
