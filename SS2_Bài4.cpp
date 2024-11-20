83316635827e0a6d2fa27a02fec8cf7ab6b19abc
minhbro122 <Lynhan.zip.zip.zip@gmail.com>
2024-11-20 17:29:30 +0700

    Add files via upload

---
+++ SS2_Bài4.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main() {
+	//canh cua hinh vuong 
+	
+	int side=10; //canh cua hinh vuong la 10
+	
+	//tinh chu vi
+	int chu_vi=4*side; 
+	
+	
+	
+	//dien tich hinh vuong
+	
+	int dien_tich=side*side; 
+	
+	printf("canh cua hinh vuong la %d \n",side);
+	printf("chu vi cua hinh vuong la %d \n",chu_vi);
+	printf("dien tich cua hinh vuong la %d \n",dien_tich);
+	
+	
+}
